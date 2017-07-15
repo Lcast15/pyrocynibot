e0e15f5cb7aa19a6ed0a6f69cd5ec6d72077b610
Lcast <Lcast15@users.noreply.github.com>
2017-07-15 21:44:10 +1000

    Deleted outdated files

--- keys.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#keys.py
-#visit http://dev.twitter.com to create an application and get your keys
-keys = dict(
-	consumer_key =			'xxxxxxxxxxxxxxxxxxxxxx',
-	consumer_secret =		'xxxxxxxxxxxxxxxxxxxxxx',
-	access_token =			'xxxxxxxxxxxxxxxxxxxxxx',
-	access_token_secret =	'xxxxxxxxxxxxxxxxxxxxxx',
-)
